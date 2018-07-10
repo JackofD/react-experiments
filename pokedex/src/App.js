476cc7794aadcc4f02bebbc51319e28db87e8efb
Deshen Padayachee <deshenx@gmail.com>
2018-07-10 10:40:54 +0200

    Added pokedex wrapper * managed to get pokedata back from request

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+//import Pokedex from 'pokedex-promise-v2';
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      pokemon: {},
+      pokeSprite: ''
+    };
+  }
+
+  componentDidMount(){
+    let idNum = (Math.floor(Math.random() * (Math.floor(802) - Math.ceil(1)) + 1));
+    const pokePromise = fetch(`http://pokeapi.co/api/v2/pokemon/${idNum}/`);
+    pokePromise
+    .then(pokedata => {
+      const pokemonData = pokedata.json();
+      return pokemonData;
+    }).then(pokemonData => {
+      this.setState({pokemon: pokemonData, pokeSprite: pokemonData.sprites.front_default});
+    }).catch(err => console.log(err));
+  }
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          Pokedata will show up here... eventually
+          <br /><br />
+          <img src={this.state.pokeSprite} alt={this.state.pokeSprite} />
+          <div>{this.state.pokemon.name}</div>
+        </div>
       </div>
     );
   }
